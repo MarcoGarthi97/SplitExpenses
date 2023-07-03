f949521223b7be44c20d5208e86d49c095cefdef
MarcoGarthi97 <marcogarthi@gmail.com>
2023-07-03 20:19:28 +0200

    Aggiungere i file di progetto.

---
+++ SplitExpenses/Models/MongoDB.cs
@@ -0,0 +1,108 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SplitExpenses.Models
+{
+    public class MongoDB
+    {
+        private string connectionString = File.ReadAllText(Properties.Settings.Default.PathCredentials + @"\Mongo.txt");
+        private IMongoDatabase GetDatabase()
+        {
+            var settings = MongoClientSettings.FromConnectionString(connectionString);
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var mongoClient = new MongoClient(settings);
+
+            return mongoClient.GetDatabase("SplitExpenses");
+        }
+
+        internal User GetUser(string username, string password)
+        {
+            try
+            {
+                IMongoDatabase splitExpenses = GetDatabase();
+                IMongoCollection<User> users = splitExpenses.GetCollection<User>("Users");
+
+                var filter = Builders<User>.Filter.Eq("Username", username);
+                filter &= Builders<User>.Filter.Eq("Password", password);
+
+                var user = users.Find(filter).FirstOrDefault();
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return null;
+            }
+        }
+
+        internal bool InsertUser(User user)
+        {
+            try
+            {
+                IMongoDatabase splitExpenses = GetDatabase();
+                IMongoCollection<User> users = splitExpenses.GetCollection<User>("Users");
+
+                users.InsertOne(user);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return false;
+            }
+        }
+
+        internal bool UpdateUser(User user)
+        {
+            try
+            {
+                IMongoDatabase splitExpenses = GetDatabase();
+                IMongoCollection<User> users = splitExpenses.GetCollection<User>("Users");
+
+                var filter = Builders<User>.Filter.Eq("Username", user.Username);
+
+                var update = Builders<User>.Update.Set(x => x, user);
+                var updateResult = users.UpdateOne(filter, update);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return false;
+            }
+        }
+
+        internal bool DeleteUser(User user)
+        {
+            try
+            {
+                IMongoDatabase splitExpenses = GetDatabase();
+                IMongoCollection<User> users = splitExpenses.GetCollection<User>("Users");
+
+                var filter = Builders<User>.Filter.Eq("Username", user.Username);
+                filter &= Builders<User>.Filter.Eq("Password", user.Password);
+
+                var delete = users.DeleteOne(filter);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return false;
+            }
+        }
+    }
+}
---
+++ SplitExpenses/Models/StructureMongo.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SplitExpenses.Models
+{
+    public class User
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId _Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string LevelUser { get; set; }
+
+        public User(string username, string password, string email, string name, DateTime birthday, string gender, string phone, string country, string levelUser)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Name = name;
+            Birthday = birthday;
+            Gender = gender;
+            Phone = phone;
+            Country = country;
+            LevelUser = levelUser;
+        }
+    }
+
+    public class Account
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId _Id { get; set; }
+        public int Id { get; set; }
+        public List<string> Users { get; set; }
+        public string Name { get; set; }
+        public double TotalExpenses { get; set; }
+        public double UserExpenses { get; set; }
+        public double Balance { get; set; }
+    }
+
+    public class Expenses
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId _Id { get; set; }
+        public int FatherId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public string PaidBy { get; set; }
+        public List<string> PaidFor { get; set; }
+        public double Cost { get; set; }
+
+    }
+}
